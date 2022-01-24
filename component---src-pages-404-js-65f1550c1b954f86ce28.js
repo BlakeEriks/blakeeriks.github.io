99a937ea9888de83d30304943aaaff3663d0080d
Blake Eriks <eblake98@vt.edu>
2022-01-24 12:14:29 -0800

    Updates

---
+++ component---src-pages-404-js-65f1550c1b954f86ce28.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblake_eriks=self.webpackChunkblake_eriks||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),u=n(3346),a=n(3751);t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-65f1550c1b954f86ce28.js.map
